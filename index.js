b0bae0478028cd7e0a88e81d3bfaa593ec5f02d5
Tsubasa1024 <inoxy1024@icloud.com>
2025-12-12 11:09:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -67,11 +67,6 @@ function mapCmsArticle(item) {
     item?.image?.url ||
     item?.heroImage?.url ||
     "images/sample1.jpg";
-// ★ アイキャッチ画像（microCMS）
-const imageUrl =
-  (item.eyecatch && item.eyecatch.url)
-  ? item.eyecatch.url
-  : "images/sample1.jpg";
 
     return {
         id: item.id,
@@ -627,6 +622,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     initAllPageSearch();
     initScrollReveal();
 });
+
 
 
 
